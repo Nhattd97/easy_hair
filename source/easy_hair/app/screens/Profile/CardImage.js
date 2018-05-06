790eb80796e55efaa4a70a92076ca3f8d35dabbb
lamhoang17797 <auroreminem@gmail.com>
2018-05-06 23:12:16 +0700

    fix

--- source/easy_hair/app/screens/Profile/CardImage.js
+++ source/easy_hair/app/screens/Profile/CardImage.js
@@ -22,9 +22,9 @@ class CardImage extends Component {
 }
 
 const styles = StyleSheet.create({
-    container: { width: 200, height: 200, backgroundColor:'white' },
-    image: { width: '100%', aspectRatio: 1 },
-    nameText: { fontWeight: 'bold' , alignSelf:'center'}
+    container: { width: 200, height: 200, backgroundColor:'white', borderWidth:5, borderColor:'#C4EBF2', borderTopRightRadius:10, borderBottomRightRadius:10 },
+    image: { width: '100%', aspectRatio: 1, borderRadius: 10, borderTopRightRadius:5, borderTopLeftRadius:1 },
+    nameText: { fontWeight: 'bold' , alignSelf:'center', color:'black'}
 })
 
 
